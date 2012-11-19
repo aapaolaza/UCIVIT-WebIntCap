9c3f9faaa026745aafc97990496a9201d0c72b88
apaolaza <apaolaza@apaolaza-OptiPlex-790>
2012-11-19 18:12:19 +0000

    This commit is the same as the previous one. Another modification that was added was that now the mouse wheel is captured. The documentation concerning this change is available on date 19/11/2012 in the modifications log.

--- UsaProxy/bin/js/log/proxyscript.js
+++ UsaProxy/bin/js/log/proxyscript.js
@@ -3,7 +3,6 @@
 //CHANGE!! I added jquery to use certain interesting functions
 //<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js" type="text/javascript"></script>
 
-
 function includeJquery(){
 //	
 //	if (typeof jQuery != 'undefined') {
